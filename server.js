16b7df63ae8a92d0f34c5e88448eb8a431286518
Harsh Singh <hsingh23@illinois.edu>
2013-04-11 15:25:34 -0500

    Added server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var locomotive = require('locomotive'),
+  address = process.env.VCAP_APP_HOST || 'localhost',
+  env = process.env.NODE_ENV || 'development',
+  port =  process.argv[2] || process.env.VMC_APP_PORT || process.env.PORT || 3000;
+
+console.log(process.argv);
+
+locomotive.boot(__dirname, env, function(err, server) {
+  if (err) { throw err; }
+  server.listen(port, address, function() {
+    var addr = this.address();
+    console.log('listening on %s:%d', addr.address, addr.port);
+  });
+});
